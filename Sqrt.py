f0f8feb05d3e84779b84c52fb96353bd64e73250
Prithwish10 <prithwishdas60@gmail.com>
2020-06-09 16:37:25 +0530

    initial commit

---
+++ Sqrt.py
@@ -0,0 +1,17 @@
+# 16 is a square of the number 4
+# 12 is not a square of any number
+
+def squarePossible(x):
+    L = 0
+    R = x
+    while L <= R:
+        mid = L + (R - L)//2
+        if mid*mid == x:
+            return mid
+        elif mid*mid < x:
+            L = mid + 1
+        else:
+            R = mid - 1
+    return -1
+
+print(squarePossible(36))
