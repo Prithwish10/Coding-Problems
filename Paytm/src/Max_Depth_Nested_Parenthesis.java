e8c66cf64f37c57702fc35e2fde70bfb3cae5b76
Prithwish10 <prithwishdas60@gmail.com>
2020-06-25 22:39:51 +0530

    25-06-2020

---
+++ Paytm/src/Max_Depth_Nested_Parenthesis.java
@@ -0,0 +1,61 @@
+//We are given a string having parenthesis like below
+//     “( ((X)) (((Y))) )”
+//We need to find the maximum depth of balanced parenthesis, like 4 in above example. Since ‘Y’ is surrounded by 4 
+//balanced parenthesis.
+//If parenthesis are unbalanced then return -1.
+
+//Input : S = "( a(b) (c) (d(e(f)g)h) I (j(k)l)m)";
+//Output : 4
+//
+//Input : S = "( p((q)) ((s)t) )";
+//Output : 3
+//
+//Input : S = "";
+//Output : 0
+//
+//Input : S = "b) (c) ()";
+//Output : -1 
+//
+//Input : S = "(b) ((c) ()"
+//Output : -1 
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Max_Depth_Nested_Parenthesis {
+
+	public static void main(String args[]) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the string :");
+		String s = sc.next();
+		
+		System.out.println(findMaxDepth(s));
+	}
+	
+	public static int findMaxDepth(String s) {
+		
+		Stack<Character> stack = new Stack<Character>();
+		
+		int max = 0;
+		
+		for(int i = 0; i < s.length(); i++) {
+			
+			if(s.charAt(i) == '(')
+				stack.add(s.charAt(i));
+			
+			else if(s.charAt(i) == ')') {
+				
+				if(stack.isEmpty())
+					return -1;
+				else
+					stack.pop();
+			}
+			else
+				max = Math.max(max, stack.size());
+		}
+		if(stack.size() == 0)
+			return max;
+		return -1;
+	}
+}
