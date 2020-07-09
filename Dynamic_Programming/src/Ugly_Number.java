b6a485c8ba419716b32b96aa235b727f54be7599
Prithwish10 <prithwishdas60@gmail.com>
2020-07-09 11:59:20 +0530

    09-07-2020

--- Dynamic_Programming/src/Ugly_Number.java
+++ Dynamic_Programming/src/Ugly_Number.java
@@ -1,31 +1,90 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Scanner;
 
 public class Ugly_Number {
 
 	public static void main(String args[]) {
 		
-		int n = 100;
-		int ar[] = new int[5];
-		Map<Integer, Integer> map = new HashMap<Integer, Integer>(5);
-		map.put(1,  8);
-		System.out.println(map.size());
-		//find(n);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter n :");
+		int n = sc.nextInt();
+		
+		System.out.println(find(n));
+		isUgly1(n);
 	}
-	public static void find(int n) {
+	//Brute force
+	
+	public static int find(int n) {
 		
 		int i = 2;
 		int c = 1;
+		
+		if(n == 1)
+			return i;
+		
 		while(true) {
 			
-			if((i % 2 == 0) || (i % 3 == 0) || (i % 5 == 0)) {
-				
-				 c ++;
-				 System.out.print(i+" ");
+			if(isUgly(i)) {
+				c ++;
+				//System.out.println(i);
 			}
 			if(c == n)
-				break;
-			i++;
+				return i;
+			
+			i ++;
 		}
+	}
+	public static boolean isUgly(int n) {
+	
+		while(n % 2 == 0 && n != 0) 
+			n /= 2;
+	
+		while(n % 3 == 0 && n != 0)
+			n /= 3;
+	
+		while(n % 5 == 0 && n != 0)
+			n /= 5;
+		
+		if(n == 1)
+			return true;
+		return false;
+	}
+	//Dp 
+	public static void isUgly1(int n) {
+		
+		int ar[] = new int[n];
+		
+		int i2 = 0;
+		int i3 = 0;
+		int i5 = 0;
+		
+		ar[0] = 1;
+		
+		int nm2 = ar[i2] * 2;;
+		int nm3 = ar[i3] * 3;
+		int nm5 = ar[i5] * 5;
+		
+		for(int i = 1; i < n; i++) {
+			
+			int min = Math.min(nm2, Math.min(nm3, nm5));
+			ar[i] = min;
+			
+			if(nm2 == min) {
+				
+				i2 = i2 + 1;
+				nm2 = ar[i2] * 2;
+			}
+			if(nm3 == min) {
+				
+				i3 = i3 + 1;
+				nm3 = ar[i3] * 3;
+			}
+			if(nm5 == min) {
+				
+				i5 = i5 + 1;
+				nm5 = ar[i5] * 5;
+			}
+		}
+		System.out.println(ar[n - 1]);
 	}
 }
