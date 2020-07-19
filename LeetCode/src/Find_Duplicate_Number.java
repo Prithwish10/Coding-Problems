35033f34af88814665f1121b27b004c9299cb824
Prithwish10 <prithwishdas60@gmail.com>
2020-07-19 22:17:02 +0530

    19-07-2020

--- LeetCode/src/Find_Duplicate_Number.java
+++ LeetCode/src/Find_Duplicate_Number.java
@@ -1,4 +1,19 @@
-
-public class Find_Duplicate_Number {
-
+class Find_Duplicate_Number {
+    public int findDuplicate(int[] nums) {
+        
+        if(nums == null || nums.length == 0)
+            return -1;
+        
+        for(int i = 0; i < nums.length; i++){
+            
+            if(nums[Math.abs(nums[i]) - 1] >= 0)
+                
+                nums[Math.abs(nums[i]) - 1] = -nums[Math.abs(nums[i]) - 1];
+            
+            else
+                
+                return Math.abs(nums[i]);
+        }
+        return -1;
+    }
 }
