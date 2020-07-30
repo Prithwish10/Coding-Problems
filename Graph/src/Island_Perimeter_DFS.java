fe4668e667ffa405c4e66ea772944fddd9e48c5a
Prithwish10 <prithwishdas60@gmail.com>
2020-07-30 23:46:23 +0530

    30-07-2020

--- Graph/src/Island_Perimeter_DFS.java
+++ Graph/src/Island_Perimeter_DFS.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
 class Island_Perimeter_DFS {
-    int count = 0;
-    public int islandPerimeter(int[][] grid) {
+    static int count = 0;
+    public static void main(String args[]) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the number of rows and columns ::");
+		int r = sc.nextInt();
+		int c = sc.nextInt();
+		
+		int grid[][] = new int[r][c];
+		
+		for(int i = 0; i < r; i++) {
+			for(int j = 0; j < c; j++) {
+				grid[i][j] = sc.nextInt();
+			}
+		}
+		
+		System.out.println(islandPerimeter(grid));
+	}
+    public static int islandPerimeter(int[][] grid) {
         boolean b = false;
         for (int i = 0; i < grid.length; i++) {
             if (b) break;
@@ -14,7 +34,7 @@ class Island_Perimeter_DFS {
         }
         return count;
     }
-    public boolean dfs(int[][] grid, int i, int j) {
+    public static boolean dfs(int[][] grid, int i, int j) {
         if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length) return true;
         if (grid[i][j] == 2) return false;
         if (grid[i][j] == 0) return true;
