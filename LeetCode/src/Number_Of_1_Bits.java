ed7cc09a4223715c0a4cd2452f3d1e38bc6e7431
Prithwish10 <prithwishdas60@gmail.com>
2020-08-24 16:59:25 +0530

    24-08-2020

---
+++ LeetCode/src/Number_Of_1_Bits.java
@@ -0,0 +1,15 @@
+
+public class Number_Of_1_Bits {
+
+public int hammingWeight(int n) {
+        
+        int count = 0;
+        
+        while(n != 0){
+            
+             n = n & (n - 1);
+             count ++;
+        }
+        return count;
+    }
+}
