b78ec58e6905aa49c01ea1a84946c92970899aff
Prithwish <prithwishdas60@gmail.com>
2020-09-27 00:19:26 +0530

    26-09-2020

--- Stack/src/Minimum_Remove_to_Make_Valid_Parentheses.java
+++ Stack/src/Minimum_Remove_to_Make_Valid_Parentheses.java
@@ -2,7 +2,7 @@ import java.util.Stack;
 
 public class Minimum_Remove_to_Make_Valid_Parentheses {
 
-public String minRemoveToMakeValid(String s) {
+	public String minRemoveToMakeValid(String s) {
         
         if(s == null || s.length() == 0)
             return "";
@@ -44,6 +44,47 @@ public String minRemoveToMakeValid(String s) {
                 
                 res += s.charAt(i);
             }
+        }
+        return res;
+    }
+	//**************** OR ***********************
+	public String minRemoveToMakeValid1(String s) {
+        
+        if(s == null || s.length() == 0)
+            return "";
+        
+        Stack<Pair> stack = new Stack<>();
+        
+        char ch[] = s.toCharArray();
+        
+        for(int i = 0; i < ch.length; i++){
+            
+            if(ch[i] == '('){
+                stack.add(new Pair(ch[i], i));
+            }
+            else if(ch[i] == ')'){
+                if(stack.isEmpty()){
+                    ch[i] = ' ';
+                }
+                else{
+                    if(stack.peek().key == '('){
+                        stack.pop();
+                    }
+                }
+            }
+        }
+        
+        while(!stack.isEmpty()){
+            
+            ch[stack.peek().value] = ' ';
+            stack.pop();
+        }
+        
+        String res = "";
+        
+        for(int i = 0; i < ch.length; i++){
+            if(ch[i] != ' ')
+                res += ch[i];
         }
         return res;
     }
