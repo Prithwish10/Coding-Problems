3a501775042af40b07416f2976ecf5c546f7e732
Prithwish <prithwishdas60@gmail.com>
2020-09-26 23:49:36 +0530

    26-09-2020

---
+++ Stack/src/Minimum_Remove_to_Make_Valid_Parentheses.java
@@ -0,0 +1,59 @@
+import java.util.Stack;
+
+public class Minimum_Remove_to_Make_Valid_Parentheses {
+
+public String minRemoveToMakeValid(String s) {
+        
+        if(s == null || s.length() == 0)
+            return "";
+        
+        Stack<Pair> stack = new Stack<>();
+        
+        for(int i = 0; i < s.length(); i++){
+            
+            char ch = s.charAt(i);
+            
+            if(ch == '('){
+                stack.add(new Pair(ch, i));
+                // sb.append(ch);
+            }
+            else if(ch == ')'){
+                if(!stack.isEmpty() && stack.peek().key == '('){
+                    //sb.append(ch);
+                    stack.pop();
+                }
+                else
+                    stack.add(new Pair(ch, i));
+            }
+           
+        }
+        
+        String res = "";
+        boolean ar[] = new boolean[s.length()];
+        
+        while(!stack.isEmpty()){
+            
+            Pair p = stack.pop();
+            int index = p.value;
+            
+            ar[index] = true;
+        }
+        for(int i = 0; i < s.length(); i++){
+            
+            if(!ar[i]){
+                
+                res += s.charAt(i);
+            }
+        }
+        return res;
+    }
+}
+class Pair{
+    
+    char key;
+    int value;
+    Pair(char key, int value){
+        this.key = key;
+        this.value = value;
+    }
+}
