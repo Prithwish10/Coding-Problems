ed305814d574e9cb888e54ea5c7001b6a2cc4dfe
Prithwish <prithwishdas60@gmail.com>
2020-10-16 02:01:34 +0530

    LeetCode(Easy)->589,590

---
+++ BT_BST/src/N_ary_PreOrder_Traversal.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class N_ary_PreOrder_Traversal {
+
+	class Node {
+	    public int val;
+	    public List<Node> children;
+
+	    public Node() {}
+
+	    public Node(int _val) {
+	        val = _val;
+	    }
+
+	    public Node(int _val, List<Node> _children) {
+	        val = _val;
+	        children = _children;
+	    }
+	}
+	List<Integer> res = new ArrayList<>();
+    
+    public List<Integer> preorder(Node root) {
+        
+        if(root == null)
+            return res;
+        helper(root);
+        return res;
+    }
+    private void helper(Node root){
+        
+        if(root == null)
+            return;
+        
+        res.add(root.val);
+        
+        List<Node> neighbour = root.children;
+        
+        for(Node child : neighbour){
+            
+            helper(child);
+        }
+    }
+}
